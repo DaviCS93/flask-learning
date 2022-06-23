e87997d522bfb0a266ced1fb31f14df5eec4b553
Davi Silva <davi.csilva00@gmail.com>
2022-06-23 13:03:17 -0300

    Class 3 Alura

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from imp import reload
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 class Game:
     def __init__(self,name,category,console):
@@ -7,16 +7,32 @@ class Game:
         self.category = category
         self.console = console
 
+game1 = Game('Tetris','Puzzle','Atari')
+game2 = Game('God of War','Hack\'n Slash','Playstation 2') 
+game3 = Game('Mortal Kombat','Fighting','Super Nintendo')
+games = [game1,game2,game3]
+
 app = Flask(__name__)
 
-@app.route('/start')
-def HelloWorld():
-    game1 = Game('Tetris','Puzzle','Atari')
-    game2 = Game('God of War','Hack\'n Slash','Playstation 2') 
-    game3 = Game('Mortal Kombat','Fighting','Super Nintendo')
-    games = [game1,game2,game3]
+@app.route('/')
+def Index():
+    variables = {'title':'Game Library'}
+    return render_template('list.html',**variables,games=games)
+ 
+@app.route('/newGame')
+def AddGame():
+    variables = {'title':'Add new game!'}
+    return render_template('newGame.html',**variables)
+
+@app.route('/create', methods=['POST',])
+def Create():
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+    newGame = Game(name,category,console)
+    games.append(newGame)
     variables = {'title':'Game Library'}
     return render_template('list.html',**variables,games=games)
  
 
-app.run()
+app.run(debug=True)
